8fdecdc25a313860aea23550ddadcbae7124c0a4
yunpeng <153967808@qq.com>
2019-02-19 18:23:47 +0800

    steps

---
+++ src/pages/component/steps.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <custom name="步骤条" bg-color="bg-gradual-pink"></custom>
+  </div>
+</template>
+
+<script>
+import Custom from "@/components/custom";
+export default {
+  data() {
+    return {};
+  },
+
+  components: { Custom },
+
+  computed: {},
+
+  methods: {},
+
+  mounted() {}
+};
+</script>
+<style lang='scss'>
+</style>
